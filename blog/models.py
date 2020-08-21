9a692b839735cd13dfe9effdc873db472f41b56e
L'oreo <l.mihaly@outlook.com>
2020-08-21 18:31:51 +0300

    done

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
     description = models.TextField()
+
+
+    def __str__(self):
+        return self.title
