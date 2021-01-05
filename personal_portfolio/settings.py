807add15970d3e73c879757d41159249ec6b0672
lori <l.mihaly@outlook.com>
2021-01-05 13:28:24 +0200

    changed134

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '%lofgdu*h7sjxg7s9rifjgh$y5ue%v)pbkpln-gxoodz!&^e#r'
 
-
-SECRET_KEY = os.getenv('SECRET_KEY')
-if not SECRET_KEY:
-    SECRET_KEY = os.environ.get('SECRET_KEY_PERSONAL_PORTFOLIO')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+# if not SECRET_KEY:
+#     SECRET_KEY = os.environ.get('SECRET_KEY_PERSONAL_PORTFOLIO')
 
 
 
